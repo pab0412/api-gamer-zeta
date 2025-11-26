ed0bf1282ba51ca1171e7f6f769b92aa74e15002
Pablo <pabl.sepulvedaa@duocuc.cl>
2025-11-26 18:03:20 -0300

    Arreglos de dependencias

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { BoletaModule } from './boleta/boleta.module';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
+      port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
