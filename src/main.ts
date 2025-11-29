7ffb16adaf062fb3b08439e2af87cfa363d34a5f
Pablo <pabl.sepulvedaa@duocuc.cl>
2025-11-29 00:44:47 -0300

    Actualizado el main.ts para funcionar con el frontend

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix('api/v1');
+
+  // Habilitar CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', // URL del frontend
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  // Validación global
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  // Swagger (OpenAPI)
+  const config = new DocumentBuilder()
+    .setTitle('API Gamer Zeta')
+    .setDescription('Cajero para Tienda Gamer Zeta')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: { persistAuthorization: true },
+  });
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log('Aplicación corriendo en: http://localhost:' + port);
+  console.log('Swagger UI:           http://localhost:' + port + '/docs');
 }
 bootstrap();
